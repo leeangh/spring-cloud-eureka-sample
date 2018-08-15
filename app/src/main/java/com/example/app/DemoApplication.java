09ce03a17ccf8138ae4d228f9f5f1e864570d693
Li An <an.li@tieto.com>
2018-08-15 14:49:07 +0800

    add web-app client

---
+++ app/src/main/java/com/example/app/DemoApplication.java
@@ -0,0 +1,42 @@
+package com.example.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@Configuration
+@EnableEurekaClient
+@EnableAutoConfiguration
+public class DemoApplication {
+
+	@Autowired
+	RestTemplate restTemplate;
+
+	private @RestController class Controller {
+
+		@RequestMapping("/sayHelo/{name}")
+		public String greeting(@PathVariable String name) {
+			return restTemplate.getForObject("http://eureka-service/greeting/{name}", String.class, name);
+		}
+
+		@Bean
+		@LoadBalanced
+		public RestTemplate restTemplate() {
+			return new RestTemplate();
+		}
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
